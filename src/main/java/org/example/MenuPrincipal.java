e409e99e429adb2463d30820e80cd1edf19d7fb4
Raphael Fagundes <rapha.fagundesm@gmail.com>
2022-06-19 22:28:54 -0300

    Initial commit

---
+++ src/main/java/org/example/MenuPrincipal.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class MenuPrincipal {
+    public static void run (){
+        System.out.println("Bem vindo ao sistema da escola, o que deseja fazer?");
+        System.out.println("1 - Cadastrar um aluno");
+        System.out.println("2 - Lançar a nota de um aluno");
+        System.out.println("3 - Exibir o boletim ");
+        System.out.println("4 - Lista de boletim a ser enviado");
+    }
+}
---
+++ src/main/java/org/example/MenuPrincipalControl.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class MenuPrincipalControl {
+    public static void run (Scanner sc) {
+        MenuPrincipal.run();
+        String opcao = sc.nextLine();
+        switch (opcao) {
+            case "1"
+        }
+    }
+}
